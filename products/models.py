509859bc4eb7fa952083cc01fa1e32260e02d5a4
Kasyanov Maxim <maxkasser90@gmail.com>
2023-10-26 01:25:30 +0300

    feat(models): add Category and Products models

--- products/models.py
+++ products/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
-# Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(max_length=100, unique=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Products(models.Model):
+    name = models.CharField('Название', max_length=255, unique=True)
+    description = models.TextField('Описание', blank=True)
+    photo = models.ImageField('Изображение', upload_to="products_photos/")
+    price = models.DecimalField('Цена', max_digits=10, decimal_places=2)
+    category = models.ForeignKey('Category', on_delete=models.SET_NULL, null=True)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = 'Товар'
+        verbose_name_plural = 'Товары'
