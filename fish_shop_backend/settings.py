cb8f6f61ca916c092e6f3c809171561baf1935fa
Leo <65715287+8evz0@users.noreply.github.com>
2023-11-10 06:35:25 +0300

    Update settings.py
    changing the database used from sqllite to Posgresql

--- fish_shop_backend/settings.py
+++ fish_shop_backend/settings.py
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'fish_shop_backend.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'db',
+        'USER': 'root',
+        'PASSWORD': 'root',
+        'HOST': 'localhost',  # Если PostgreSQL находится на том же сервере, что и Django
+        'PORT': '5432',           # По умолчанию, PostgreSQL использует порт 5432
     }
 }
 
