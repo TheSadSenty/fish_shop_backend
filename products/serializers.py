2bfcb12f208d71b0d0181aa678cc4c995efdad97
Kasyanov Maxim <maxkasser90@gmail.com>
2023-11-18 16:30:46 +0300

    feat!: add nested serialization
    Now ProductsSerializer uses DiscountSerializer and CategorySerializer to serialize ForeignKey
    Now CategorySerializer uses DiscountSerializer to serialize ForeignKey

--- products/serializers.py
+++ products/serializers.py
@@ -1,15 +1,26 @@
 from rest_framework import serializers
-from products.models import Category, Products
+from products.models import Category, Products, Discount
+
+
+class DiscountSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Discount
+        fields = '__all__'
+
+
+class CategorySerializer(serializers.ModelSerializer):
+    discount = DiscountSerializer(read_only=True)
+
+    class Meta:
+        model = Category
+        fields = ['name', 'discount']
 
 
 class ProductsSerializer(serializers.ModelSerializer):
+    discount = DiscountSerializer(read_only=True)
+    category = CategorySerializer(read_only=True)
+
     class Meta:
         model = Products
         fields = ['id', 'name', 'description', 'photo',
                   'price', 'category', 'amount', 'discount']
-
-
-class CategorySerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Category
-        fields = ['name', 'discount']
