60b31e8ec5e4e918f70e1904eeb8e18c341e45b4
Kasyanov Maxim <maxkasser90@gmail.com>
2023-11-20 18:50:12 +0300

    refactor: minor change in admin site in cart section

--- cart/admin.py
+++ cart/admin.py
@@ -1,7 +1,4 @@
 from django.contrib import admin
 from .models import Cart
 
-class CartAdmin(admin.ModelAdmin):
-    readonly_fields = ('category',)
-
-admin.site.register(Cart, CartAdmin)
+admin.site.register(Cart)
