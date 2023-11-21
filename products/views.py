ced06af3134f6fe8a2ad8eef35d4fedb1a6b709e
Kasyanov Maxim <maxkasser90@gmail.com>
2023-11-21 08:03:17 +0300

    feat: add PageNumberPagination
    Now ProductViewSet support pagination
    Now CategoryViewSet support pagination

--- products/views.py
+++ products/views.py
@@ -1,6 +1,7 @@
 from rest_framework.renderers import JSONRenderer
 from rest_framework.response import Response
 from rest_framework import viewsets
+from rest_framework.pagination import PageNumberPagination
 from django.shortcuts import get_object_or_404
 from products.models import Category, Products
 from products.serializers import ProductsSerializer, CategorySerializer
@@ -8,29 +9,13 @@ from products.serializers import ProductsSerializer, CategorySerializer
 
 class ProductViewSet(viewsets.ReadOnlyModelViewSet):
     renderer_classes = [JSONRenderer]
-
-    def list(self, request):
-        queryset = Products.objects.all()
-        serializer = ProductsSerializer(queryset, many=True)
-        return Response(serializer.data)
-
-    def retrieve(self, request, pk=None):
-        queryset = Products.objects.all()
-        product = get_object_or_404(queryset, pk=pk)
-        serializer = ProductsSerializer(product)
-        return Response(serializer.data)
+    pagination_class = PageNumberPagination
+    queryset = Products.objects.all()
+    serializer_class = ProductsSerializer
 
 
 class CategoryViewSet(viewsets.ReadOnlyModelViewSet):
     renderer_classes = [JSONRenderer]
-
-    def list(self, request):
-        queryset = Category.objects.all()
-        serializer = CategorySerializer(queryset, many=True)
-        return Response(serializer.data)
-
-    def retrieve(self, request, pk=None):
-        queryset = Category.objects.all()
-        category = get_object_or_404(queryset, pk=pk)
-        serializer = CategorySerializer(category)
-        return Response(serializer.data)
+    pagination_class = PageNumberPagination
+    queryset = Category.objects.all()
+    serializer_class = CategorySerializer
