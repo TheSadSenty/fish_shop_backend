43b642c20867c99c9050a64996a8f31858ad1363
Kasyanov Maxim <kasyanov.maxim1000@gmail.com>
2023-12-05 18:38:12 +0300

    feat: create FavoriteProduct model
    Add product and user field in it

--- users/models.py
+++ users/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from products.models import Products
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class FavoriteProduct(models.Model):
+    product = models.ForeignKey(
+        Products, on_delete=models.CASCADE, verbose_name="Продукт")
+    user = models.ForeignKey(
+        User, on_delete=models.CASCADE, verbose_name="Пользователь")
+
+    class Meta:
+        verbose_name = 'Любимый продукт'
+        verbose_name_plural = 'Любимые продукты'
