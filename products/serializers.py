dea7829cf42f095d3f1a4c74be00fb46b56fdbe1
Kasyanov Maxim <kasyanov.maxim1000@gmail.com>
2023-12-07 00:55:50 +0300

    refactor: completely delete Discount model
    Delete related functionality

--- products/serializers.py
+++ products/serializers.py
@@ -1,29 +1,18 @@
 from rest_framework import serializers
-from products.models import Category, Products, Discount
-
-
-class DiscountSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Discount
-        fields = '__all__'
+from products.models import Category, Products
 
 
 class CategorySerializer(serializers.ModelSerializer):
-    # discount = DiscountSerializer(read_only=True)
 
     class Meta:
         model = Category
-        # fields = ['name', 'discount']
         fields = ['name']
 
 
 class ProductsSerializer(serializers.ModelSerializer):
-    # discount = DiscountSerializer(read_only=True)
     category = CategorySerializer(read_only=True)
 
     class Meta:
         model = Products
-        # fields = ['id', 'name', 'description', 'photo',
-        #          'price', 'category', 'amount', 'discount']
         fields = ['id', 'name', 'description', 'photo',
                   'price', 'category', 'amount']
