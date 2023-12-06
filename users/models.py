c10e64316d6c868c4a822c4e739e889988551bab
Kasyanov Maxim <kasyanov.maxim1000@gmail.com>
2023-12-06 18:58:22 +0300

    feat: create UserReview model

--- users/models.py
+++ users/models.py
@@ -12,3 +12,24 @@ class FavoriteProduct(models.Model):
     class Meta:
         verbose_name = 'Любимый продукт'
         verbose_name_plural = 'Любимые продукты'
+
+
+class UserReview(models.Model):
+    user = models.ForeignKey(
+        User, on_delete=models.CASCADE, verbose_name="Пользователь")
+    user_public_name = models.CharField(
+        verbose_name="Публичное имя пользователя", default="Анонимный пользователь")
+    text = models.CharField(verbose_name="Отзыв")
+    product = models.ForeignKey(
+        Products, on_delete=models.SET_NULL, verbose_name="Продукт", null=True)
+    is_anonymous = models.BooleanField(
+        verbose_name="Анонимный отзыв", default=True)
+    created_at = models.DateTimeField(
+        verbose_name="Дата создания", auto_now_add=True)
+
+    def save(self, *args, **kwargs):
+        if self.is_anonymous == False:
+            self.user_public_name = self.user.last_name+" "+self.user.first_name
+            super().save(*args, **kwargs)
+        else:
+            super().save(*args, **kwargs)
