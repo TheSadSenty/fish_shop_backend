940ca89449e63c1be3fa697398de9c5953a937df
Kasyanov Maxim <kasyanov.maxim1000@gmail.com>
2023-12-09 04:05:13 +0300

    fix: add several CORS headers
    Add CORS_ALLOW_HEADERS
    Add CORS_EXPOSE_HEADERS
    Add SECURE_CROSS_ORIGIN_OPENER_POLICY

--- fish_shop_backend/settings.py
+++ fish_shop_backend/settings.py
@@ -13,6 +13,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 from pathlib import Path
 import os
 from decouple import config
+from corsheaders.defaults import default_headers
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -45,11 +46,11 @@ INSTALLED_APPS = [
     'rest_framework.authtoken',
     'corsheaders'
 ]
-
+CORS_ALLOW_ALL_ORIGINS = True
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
-    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -148,8 +149,10 @@ REST_FRAMEWORK = {
     ]
 }
 CORS_ALLOW_HEADERS = [
+    *default_headers,
+    "Authorization",
+]
+CORS_EXPOSE_HEADERS = [
     "Auth-Token",
 ]
-CORS_ALLOWED_ORIGINS = [
-    "http://localhost:4200",
-]
+SECURE_CROSS_ORIGIN_OPENER_POLICY = None
