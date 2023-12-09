6f66db40811ec63171d2fcd52b5d00670f84bdb5
Kasyanov Maxim <kasyanov.maxim1000@gmail.com>
2023-12-09 03:18:37 +0300

    fix: return django-cors-headers into dependencies
    Add CORS_ALLOW_HEADERS and CORS_ALLOWED_ORIGINS

--- fish_shop_backend/settings.py
+++ fish_shop_backend/settings.py
@@ -42,12 +42,14 @@ INSTALLED_APPS = [
     'products',
     'cart',
     'users',
-    'rest_framework.authtoken'
+    'rest_framework.authtoken',
+    'corsheaders'
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -145,3 +147,9 @@ REST_FRAMEWORK = {
         'rest_framework.authentication.TokenAuthentication',
     ]
 }
+CORS_ALLOW_HEADERS = [
+    "Auth-Token",
+]
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:4200",
+]
